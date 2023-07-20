3624db6c46f3bed6c5a94462c14d57aebd755c41
akash <akashkatare692@gmail.com>
2023-07-20 18:24:50 +0530

    menu comp

--- src/app/component/menu/menu.component.ts
+++ src/app/component/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from '../login/login.component';
 import { RegisterComponent } from '../register/register.component';
+import { HardcodedAuthenticationService } from 'src/app/service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,8 +11,31 @@ import { RegisterComponent } from '../register/register.component';
 })
 export class MenuComponent {
 
-  constructor(private dialog:MatDialog){}
+  // constructor(private dialog:MatDialog, public hardcodedAuthentication:HardcodedAuthenticationService){}
 
+  // //for login popup
+  // Openpopup(){
+  //   this.dialog.open(LoginComponent,{
+  //     width:'60%',
+  //     height:'400px'
+  //   })
+  // }
+
+  // //for sign in popup
+  // Openpopup2(){
+  //   this.dialog.open(RegisterComponent,{
+  //     width:'50%',
+  //     height:'500px'
+  //   })
+  // }
+
+  isUserLoggedIn:boolean=false;
+
+  constructor(private dialog:MatDialog, public hardcodedAuthentication:HardcodedAuthenticationService){}
+
+  ngOnInit(): void {
+    this.isUserLoggedIn=this.hardcodedAuthentication.isUserLoggedIn();
+   }
   //for login popup
   Openpopup(){
     this.dialog.open(LoginComponent,{
@@ -27,5 +51,6 @@ export class MenuComponent {
       height:'500px'
     })
   }
+
 
 }
